f966ffb9c06b56a4c0636834d5ffa3ece4e10d50
KZ <zhiping72@gmail.com>
2015-11-22 01:21:35 +0100

    Try react with jspm

--- Orderstraat/index.js
+++ Orderstraat/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Title from 'components/Header.js!jsx';
+
 ReactDOM.render(
-    <h1>Hello world</h1>,
+    <Title />,
     document.querySelector('.root')
 );
